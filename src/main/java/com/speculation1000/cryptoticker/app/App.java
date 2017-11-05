ee7efbc4208ffd41e8ce32382cdbdb3e90334a25
yguy08 <wyatt.endres@gmail.com>
2017-11-05 16:21:06 -0500

    config

--- src/main/java/com/speculation1000/cryptoticker/app/App.java
+++ src/main/java/com/speculation1000/cryptoticker/app/App.java
@@ -20,7 +20,7 @@ public class App {
 	
 	public App() throws Exception {	
 		exchanges = new ArrayList<>();
-	    ResultSet rs = new Csv().read("exchange.csv", null, null);
+	    ResultSet rs = new Csv().read("src/main/resources/exchange.csv", null, null);
         
 	    while (rs.next()) {
         	exchanges.add(rs.getString(1));
