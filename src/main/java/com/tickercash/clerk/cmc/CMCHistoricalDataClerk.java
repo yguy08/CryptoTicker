381262cf51e7adf5db94c5badfb7aea95e52571e
Wyatt Endres <wyatt.endres@gmail.com>
2018-03-05 17:44:56 -0500

    cmc historical data

--- src/main/java/com/tickercash/clerk/cmc/CMCHistoricalDataClerk.java
+++ src/main/java/com/tickercash/clerk/cmc/CMCHistoricalDataClerk.java
@@ -1,55 +1,44 @@
 package com.tickercash.clerk.cmc;
 
+import java.io.IOException;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.ZoneOffset;
-import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.jsoup.Jsoup;
-
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
 import com.tickercash.clerk.HistoricalDataClerk;
 import com.tickercash.marketdata.Tick;
+import com.tickercash.util.DateTimePatterns;
 
 public class CMCHistoricalDataClerk implements HistoricalDataClerk {
-	
-	private static final DateTimeFormatter cmcDateFormat = DateTimeFormatter.ofPattern("MMM dd, yyyy");
-	private static final String cmcHistoricalDateElementId = "historical-data";
-	
-	public CMCHistoricalDataClerk() {}
-
-	@Override
-	public List<Tick> getHistoricalTicks(String symbol, long start, long end) {
-		return null;
-	}
-	
-	@Override
-	public List<Tick> getHistoricalTicks(String symbol, String start, String end) throws Exception {
-		String url = "https://coinmarketcap.com/currencies/"+symbol+"/historical-data/?start="+start+"&end="+end;
-		return Jsoup.connect(url).get()
-		        .getElementById(cmcHistoricalDateElementId)
-		        .getElementsByTag("tr")
-		        .stream()
-		        .skip(1)
-		        .map(td -> td.getElementsByTag("td"))
-		        .map(t -> new Tick(
-		        		symbol, 
-		        		cmcDateToEpochSeconds(t.get(0).text()), 
-		        		Double.parseDouble(t.get(4).text())))
-		        .collect(Collectors.toList());
-	}
-	
-	/**
-	 * Returns epoch seconds from CMC historical format date (MMM dd, yyyy) 
-	 * @param date in MMM dd, yyyy format
-	 * @return epoch seconds
-	 */
-	private static long cmcDateToEpochSeconds(String dateStr){
-		LocalDate localDate = LocalDate.parse(dateStr, cmcDateFormat);
-		LocalDateTime localDateTime = LocalDateTime.of(localDate, LocalTime.MIDNIGHT);
-		return localDateTime.toEpochSecond(ZoneOffset.UTC);
-	}
+    
+    @Override
+    public List<Tick> getHistoricalTicks(String symbol, LocalDateTime startDate, LocalDateTime endDate) throws IOException {
+        String start = startDate.format(DateTimePatterns.YYYYMMDD);
+        String end = endDate.format(DateTimePatterns.YYYYMMDD);
+        String url = "https://coinmarketcap.com/currencies/"+symbol+"/historical-data/?start="+start+"&end="+end;
+        Document document = Jsoup.connect(url).get();
+        Elements elements = document.getElementById("historical-data").getElementsByTag("tr");
+        return  elements.stream().skip(1)
+                .map(td -> td.getElementsByTag("td"))
+                .map(t -> new Tick(symbol, cmcDateToEpochSeconds(t.get(0).text()), Double.parseDouble(t.get(4).text())))
+                .collect(Collectors.toList());
+    }
+    
+    /**
+     * Returns epoch seconds from CMC historical format date (MMM dd, yyyy) 
+     * @param date in MMM dd, yyyy format
+     * @return epoch seconds
+     */
+    private static long cmcDateToEpochSeconds(String dateStr){
+        LocalDate localDate = LocalDate.parse(dateStr, DateTimePatterns.MMM_dd_COMMA_yyyy);
+        LocalDateTime localDateTime = LocalDateTime.of(localDate, LocalTime.MIDNIGHT);
+        return localDateTime.toEpochSecond(ZoneOffset.UTC);
+    }
 
 }
