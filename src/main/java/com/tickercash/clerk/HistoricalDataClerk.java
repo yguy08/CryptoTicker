cacf5c1b35311dc98dc358269b8c534e2140a3a5
Wyatt Endres <wyatt.endres@gmail.com>
2018-03-05 17:38:10 -0500

    get hist ticks localdate time params

--- src/main/java/com/tickercash/clerk/HistoricalDataClerk.java
+++ src/main/java/com/tickercash/clerk/HistoricalDataClerk.java
@@ -1,13 +1,12 @@
 package com.tickercash.clerk;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import com.tickercash.marketdata.Tick;
 
 public interface HistoricalDataClerk {
     
-    public List<Tick> getHistoricalTicks(String symbol, long start, long end);
+    public List<Tick> getHistoricalTicks(String currency, LocalDateTime startDate, LocalDateTime endDate) throws Exception;
     
-    public List<Tick> getHistoricalTicks(String symbol, String start, String end) throws Exception;
-
 }
