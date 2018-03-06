350c6362139a2b8b56933a4a8b5eed0cf37f09a3
Wyatt Endres <wyatt.endres@gmail.com>
2018-03-06 17:58:18 -0500

    cmc historical test

--- src/test/java/com/tickercash/clerk/cmc/CMCHistoricalDataClerkTest.java
+++ src/test/java/com/tickercash/clerk/cmc/CMCHistoricalDataClerkTest.java
@@ -1,20 +1,22 @@
 package com.tickercash.clerk.cmc;
 
-import java.time.LocalDateTime;
+import static org.junit.Assert.assertNotNull;
 
+import java.time.LocalDateTime;
 import org.junit.Test;
 
 import com.tickercash.clerk.HistoricalDataClerk;
 
 public class CMCHistoricalDataClerkTest {
-	
-	private static LocalDateTime testDate = LocalDateTime.of(2018,3,5,8,55);
+    
+    private static LocalDateTime testDate = LocalDateTime.of(2018,3,5,8,55);
 
-	@Test
-	public void testBitcoinHistorical() throws Exception {
-		HistoricalDataClerk hdc = new CMCHistoricalDataClerk();
-		hdc.getHistoricalTicks("Bitcoin", testDate.minusYears(5), testDate)
-		.stream().forEach(System.out::println);		
-	}
-	
+    @Test
+    public void testBitcoinHistorical() throws Exception {
+        HistoricalDataClerk hdc = new CMCHistoricalDataClerk();
+        hdc.getHistoricalTicks("Bitcoin", testDate.minusYears(5), testDate)
+           .stream()
+           .forEach(t -> assertNotNull(t));
+    }
+    
 }
