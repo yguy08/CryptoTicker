120f659260141e44f31deddbd3ca9b6063518a14
Wyatt Endres <wyatt.endres@gmail.com>
2018-03-14 18:25:54 -0400

    working with server starter

--- src/main/java/com/tickercash/window/ServerStarter.java
+++ src/main/java/com/tickercash/window/ServerStarter.java
@@ -1,45 +1,46 @@
 package com.tickercash.window;
 
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
-import com.googlecode.lanterna.graphics.TextGraphics;
+import java.util.Arrays;
+import com.googlecode.lanterna.gui2.BasicWindow;
+import com.googlecode.lanterna.gui2.GridLayout;
+import com.googlecode.lanterna.gui2.Label;
+import com.googlecode.lanterna.gui2.MultiWindowTextGUI;
+import com.googlecode.lanterna.gui2.Panel;
+import com.googlecode.lanterna.gui2.Window;
+import com.googlecode.lanterna.gui2.Window.Hint;
+import com.googlecode.lanterna.gui2.WindowBasedTextGUI;
 import com.googlecode.lanterna.screen.Screen;
-import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
-import com.googlecode.lanterna.terminal.Terminal;
 
 public class ServerStarter {
 
-	public static void main(String[] args) throws Exception {
-		Terminal terminal = new DefaultTerminalFactory().createTerminal();
-		Screen screen = new TerminalScreen(terminal);		
-		
-		TextGraphics tGraphics = screen.newTextGraphics();
-		
-		screen.startScreen();
-		screen.clear();
-		screen.setCursorPosition(null);
-		
-		String welcome = Stream.of(args).collect(Collectors.joining(" "));
-		for(int i = 0; i < welcome.length(); i++){
-			tGraphics.setCharacter(i, 1, welcome.charAt(i));
-			slowType();
-			screen.refresh();
-		}
-
-		screen.readInput();
-		screen.stopScreen();
-		screen.close();
-
-	}
-	
-	private static void slowType() {
-		try {
-			Thread.sleep(100);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-	}
+    public static void main(String[] args) throws Exception {
+        DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory();
+        Screen screen = terminalFactory.createScreen();
+        screen.startScreen();
+        
+        final WindowBasedTextGUI textGUI = new MultiWindowTextGUI(screen);
+        final Window window = new BasicWindow("Market Data Server");
+        window.setHints(Arrays.asList(Hint.CENTERED));
+        
+        Panel contentPanel = new Panel(new GridLayout(3));
+        
+        GridLayout gridLayout = (GridLayout)contentPanel.getLayoutManager();
+        gridLayout.setHorizontalSpacing(4);
+        
+        Label liveFeed = new Label("Market Data");
+        contentPanel.addComponent(liveFeed);
+        
+        Label historicalFeed = new Label("Broker");
+        contentPanel.addComponent(historicalFeed);
+        
+        Label subscriptions = new Label("Data Source");
+        contentPanel.addComponent(subscriptions);
+        
+        window.setComponent(contentPanel);
+        textGUI.addWindowAndWait(window);
+        
+        
+    }
 
 }
