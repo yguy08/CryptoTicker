bf7d1a3bd86421ca264a473122d0628f74dee7cd
Wyatt Endres <wyatt.endres@gmail.com>
2018-03-23 17:18:45 -0400

    Add files via upload

--- src/main/java/com/tickercash/event/translator/MarketDataTranslator.java
+++ src/main/java/com/tickercash/event/translator/MarketDataTranslator.java
@@ -3,24 +3,33 @@ package com.tickercash.event.translator;
 import org.knowm.xchange.coinmarketcap.dto.marketdata.CoinMarketCapTicker;
 import org.knowm.xchange.dto.marketdata.Ticker;
 
-import com.tickercash.event.MarketDataEvent;
-import com.tickercash.marketdata.FakeTick;
 import com.tickercash.marketdata.Tick;
 import com.tickercash.util.UniqueCurrentTimeMS;
 
+import net.openhft.chronicle.bytes.Bytes;
+import net.openhft.chronicle.bytes.StopCharTesters;
+
+import java.nio.ByteBuffer;
+
 public class MarketDataTranslator {
     
-    public static final void translateTo(MarketDataEvent event, long sequence, CoinMarketCapTicker ticker) {
-        event.set(new Tick(ticker.getID().toUpperCase()+"/BTC", ":CMC", 
-        		UniqueCurrentTimeMS.uniqueCurrentTimeMS(), ticker.getPriceBTC().doubleValue()));
+    private static Bytes<ByteBuffer> bytes;
+    
+    public static final void translateTo(Tick event, long sequence, CoinMarketCapTicker ticker) {
+        event.set(ticker.getID().toUpperCase()+"/BTC", ":CMC", UniqueCurrentTimeMS.uniqueCurrentTimeMS(), ticker.getPriceBTC().doubleValue());
     }
     
-    public static final void translateTo(MarketDataEvent event, long sequence, String feed, Ticker ticker) {
-        event.set(new Tick(ticker.getCurrencyPair().toString(), feed, ticker.getTimestamp().getTime(), ticker.getLast().doubleValue()));
+    public static final void translateTo(Tick event, long sequence, String feed, Ticker ticker) {
+        event.set(ticker.getCurrencyPair().toString(), feed, ticker.getTimestamp().getTime(), ticker.getLast().doubleValue());
     }
     
-    public static final void translateToFakeTick(MarketDataEvent event, long sequence){
-        event.set(new FakeTick());
+    public static final void translateTo(Tick event, long sequence, String[] strArr, long ts, double last){
+        event.set(strArr[0], strArr[1], ts, last);
+    }
+    
+    public static final void translateTo(Tick event, long sequence, String tick){
+        bytes = Bytes.elasticByteBuffer().append8bit(tick);
+        event.set(bytes.parseUtf8(StopCharTesters.SPACE_STOP), bytes.parseUtf8(StopCharTesters.SPACE_STOP), bytes.parseLong(), bytes.parseDouble());
     }
 
 }
