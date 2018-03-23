c70a27ddd38b28cb61675886e8bc6bee182db50c
Wyatt Endres <wyatt.endres@gmail.com>
2018-03-23 17:19:55 -0400

    Add files via upload

--- src/main/java/com/tickercash/marketdata/Tick.java
+++ src/main/java/com/tickercash/marketdata/Tick.java
@@ -2,25 +2,80 @@ package com.tickercash.marketdata;
 
 import com.tickercash.util.NumberFormatPatterns;
 
-public class Tick extends MarketDataEventVO {
+public class Tick {
     
+    private String symbol;
+    private String feed;
+    private long timestamp;
     private double last;
+    private int volume;
     
-    public Tick(String symbol, String feed, long timestamp, double last){
-        super(symbol, feed, timestamp);
-    	this.last = last;
+    public Tick(){
+        // For Disruptor Factory
     }
     
+    public Tick(String symbol, String feed, long timestamp, double last){
+        this.symbol = symbol;
+        this.feed = feed;
+        this.timestamp = timestamp;
+        this.last = last;
+    }
+    
+    public Tick(String symbol, String feed, long timestamp, double last, int volume){
+        this.symbol = symbol;
+        this.feed = feed;
+        this.timestamp = timestamp;
+        this.last = last;
+        this.volume = volume;
+    }
+    
+    public Tick set(String symbol, String feed, long timestamp, double last){
+        this.symbol = symbol;
+        this.feed = feed;
+        this.timestamp = timestamp;
+        this.last = last;
+        return this;
+    }
+    
+    public Tick set(String symbol, String feed, long timestamp, double last, int volume){
+        this.symbol = symbol;
+        this.feed = feed;
+        this.timestamp = timestamp;
+        this.last = last;
+        this.volume = volume;
+        return this;
+    }
+
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public void setFeed(String feed) {
+        this.feed = feed;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+    
+    public String getFeed() {
+        return feed;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
     public double getLast(){
         return last;
     }
-
-    public void setLast(double last) {
-        this.last = last;
+    
+    public int getVolume() {
+        return volume;
     }
     
     @Override
     public String toString() {
-        return super.toString()+" "+NumberFormatPatterns.SAT_FORMAT.format(getLast());
+        return this.symbol+" "+this.feed+" "+this.timestamp+" "+NumberFormatPatterns.SAT_FORMAT.format(this.last);
     }
 }
