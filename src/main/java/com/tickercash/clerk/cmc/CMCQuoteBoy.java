c403c38cfe570415d70d7e9c07b254522d1a6824
Wyatt Endres <wyatt.endres@gmail.com>
2018-03-23 17:17:18 -0400

    Add files via upload

--- src/main/java/com/tickercash/clerk/cmc/CMCQuoteBoy.java
+++ src/main/java/com/tickercash/clerk/cmc/CMCQuoteBoy.java
@@ -1,10 +1,5 @@
 package com.tickercash.clerk.cmc;
 
-import java.io.IOException;
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.knowm.xchange.Exchange;
@@ -13,42 +8,47 @@ import org.knowm.xchange.coinmarketcap.CoinMarketCapExchange;
 import org.knowm.xchange.coinmarketcap.service.CoinMarketCapMarketDataService;
 
 import com.tickercash.clerk.QuoteBoy;
+import com.tickercash.enums.QuoteBoyType;
 import com.tickercash.event.translator.MarketDataTranslator;
 
 public class CMCQuoteBoy extends QuoteBoy {
-	    
+        
     private final Exchange CMC_EXCHANGE;
     
     private final CoinMarketCapMarketDataService CMC_MARKET_DATA_SERVICE;
     
-    private int throttle = 1000;
+    private static final Logger LOGGER = LogManager.getLogger("CMCQuoteBoy");
     
-    private static final Logger LOGGER = LogManager.getLogger("XchangeTicker");
-	
-	public CMCQuoteBoy() {
-		CMC_EXCHANGE = ExchangeFactory.INSTANCE.createExchange(CoinMarketCapExchange.class.getName());
-		CMC_MARKET_DATA_SERVICE = (CoinMarketCapMarketDataService) CMC_EXCHANGE.getMarketDataService();		
-	}
-	
-	public CMCQuoteBoy(int seconds) {
-		CMC_EXCHANGE = ExchangeFactory.INSTANCE.createExchange(CoinMarketCapExchange.class.getName());
-		CMC_MARKET_DATA_SERVICE = (CoinMarketCapMarketDataService) CMC_EXCHANGE.getMarketDataService();
-		throttle = seconds*1000;
-	}
+    public CMCQuoteBoy() {
+        CMC_EXCHANGE = ExchangeFactory.INSTANCE.createExchange(CoinMarketCapExchange.class.getName());
+        CMC_MARKET_DATA_SERVICE = (CoinMarketCapMarketDataService) CMC_EXCHANGE.getMarketDataService();
+        throttle = 5000;
+    }
+    
+    public CMCQuoteBoy(int seconds) {
+        CMC_EXCHANGE = ExchangeFactory.INSTANCE.createExchange(CoinMarketCapExchange.class.getName());
+        CMC_MARKET_DATA_SERVICE = (CoinMarketCapMarketDataService) CMC_EXCHANGE.getMarketDataService();
+        throttle = seconds*1000;
+    }
 
-	@Override
-	public void start() throws Exception {
-		disruptor.start();
-		while(true) {
-			try {
-				CMC_MARKET_DATA_SERVICE.getCoinMarketCapTickers(100).stream().forEach((s) 
-						-> ringBuffer.publishEvent(MarketDataTranslator::translateTo, s));
-				Thread.sleep(throttle);
-			} catch (Exception e) {
-				LOGGER.error(e);
-				Thread.sleep(throttle);
-			}
-		}		
-	}
+    @Override
+    public void start() throws Exception {
+        disruptor.start();
+        while(true) {
+            try {
+                CMC_MARKET_DATA_SERVICE.getCoinMarketCapTickers(100).stream().forEach((s) 
+                        -> ringBuffer.publishEvent(MarketDataTranslator::translateTo, s));
+                Thread.sleep(throttle);
+            } catch (Exception e) {
+                LOGGER.error(e);
+                Thread.sleep(throttle);
+            }
+        }
+    }
+
+    @Override
+    public String getName() {
+        return QuoteBoyType.CMC.toString();
+    }
 
 }
