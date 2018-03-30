bca5772194cf99f3137fcc44e3ceb090b4f85f9c
Wyatt Endres <wyatt.endres@gmail.com>
2018-03-30 13:43:17 -0400

    non persistent

--- src/main/java/com/tickercash/tapereader/wire/Transmitter.java
+++ src/main/java/com/tickercash/tapereader/wire/Transmitter.java
@@ -4,6 +4,7 @@ import com.lmax.disruptor.EventHandler;
 import com.tickercash.tapereader.marketdata.Tick;
 
 import javax.jms.Connection;
+import javax.jms.DeliveryMode;
 import javax.jms.JMSException;
 import javax.jms.MessageProducer;
 import javax.jms.Session;
@@ -13,31 +14,32 @@ import org.apache.logging.log4j.Logger;
 
 public class Transmitter implements EventHandler<Tick> {
       
-	private Connection connection;
-	  
-	private Session session;
-	  
-	private MessageProducer messageProducer;
-	  
-	private static final Logger LOGGER = LogManager.getLogger("Transmitter");
-	  
-	public Transmitter(String topicStr) throws Exception {
-	      MQBroker.initDefaultMQBroker();
-	      connection = new ActiveMQConnectionFactory("tcp://localhost:61616").createConnection();
-	      session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-	      messageProducer = session.createProducer(session.createTopic(topicStr));
-	}
+    private Connection connection;
+      
+    private Session session;
+      
+    private MessageProducer messageProducer;
+      
+    private static final Logger LOGGER = LogManager.getLogger("Transmitter");
+      
+    public Transmitter(String topicStr) throws Exception {
+          MQBroker.initDefaultMQBroker();
+          connection = new ActiveMQConnectionFactory("tcp://localhost:61616").createConnection();
+          session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+          messageProducer = session.createProducer(session.createTopic(topicStr));
+          messageProducer.setDeliveryMode(DeliveryMode.NON_PERSISTENT);
+    }
 
-	public void closeConnection() throws JMSException {
-	    connection.close();
-	}
-	
-	@Override
-	public void onEvent(Tick event, long sequence, boolean endOfBatch) throws Exception {
-	    try{
-	        messageProducer.send(session.createTextMessage(event.toString()));
-	    }catch(Exception e){
-	        LOGGER.error(e.getMessage());
-	    }
-	}    
+    public void closeConnection() throws JMSException {
+        connection.close();
+    }
+    
+    @Override
+    public void onEvent(Tick event, long sequence, boolean endOfBatch) throws Exception {
+        try{
+            messageProducer.send(session.createTextMessage(event.toString()));
+        }catch(Exception e){
+            LOGGER.error(e.getMessage());
+        }
+    }
 }
