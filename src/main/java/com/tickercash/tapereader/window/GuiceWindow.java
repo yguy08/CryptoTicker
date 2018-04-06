23deaea327bf40dc5d69a70e66971dce6855bbf3
Wyatt Endres <wyatt.endres@gmail.com>
2018-04-06 17:12:49 -0400

    Add files via upload

---
+++ src/main/java/com/tickercash/tapereader/window/GuiceWindow.java
@@ -0,0 +1,16 @@
+package com.tickercash.tapereader.window;
+
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import com.tickercash.tapereader.TapeReader;
+import com.tickercash.tapereader.TapeReaderModule;
+
+public class GuiceWindow {
+
+    public static void main(String[] args) throws Exception {
+        Injector injector = Guice.createInjector(new TapeReaderModule());
+        TapeReader reader = injector.getInstance(TapeReader.class);
+        reader.readTheTape();
+    }
+
+}
