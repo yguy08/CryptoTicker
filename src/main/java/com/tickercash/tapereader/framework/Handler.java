90dc6dc62f778f5f28a78c124db387e5687cca52
Wyatt Endres <wyatt.endres@gmail.com>
2018-04-10 16:39:45 -0400

    Delete Handler.java

--- src/main/java/com/tickercash/tapereader/framework/Handler.java
+++
@@ -1,7 +0,0 @@
-package com.tickercash.tapereader.framework;
-
-import com.tickercash.tapereader.model.Tick;
-
-public interface Handler {
-    public void onEvent(Tick event, long sequence, boolean endOfBatch) throws Exception;
-}
