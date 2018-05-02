db419f136df9855a1a5cd68cebbeb6cfd3e7dd7e
Wyatt Endres <wyatt.endres@gmail.com>
2018-05-02 11:48:09 -0400

    Add files via upload

--- src/main/java/com/tapereader/ticker/TickerType.java
+++ src/main/java/com/tapereader/ticker/TickerType.java
@@ -1,26 +1,14 @@
 package com.tapereader.ticker;
 
-import com.tapereader.gui.Displayable;
 
-public enum TickerType implements Displayable {
+public enum TickerType {
     
-    POLONIEX("Poloniex"),
-    BINANCE("Binance"),
-    CMC("CMC"),
-    GDAX("GDAX"),
-    KUCOIN("Kucoin"),
-    FAKE("Fake"),
-    CSV("Csv");
-	
-    private String displayName;
-    
-    TickerType(String displayName){
-        this.displayName = displayName;
-    }
-
-    @Override
-    public String getDisplayName() {
-        return displayName;
-    }
+    POLONIEX,
+    BINANCE,
+    CMC,
+    GDAX,
+    KUCOIN,
+    FAKE,
+    CSV;
 
 }
