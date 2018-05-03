c7ef7c95c701f6d992018c633e4c92e7dd2e7554
Wyatt Endres <wyatt.endres@gmail.com>
2018-05-03 17:43:23 -0400

    Add files via upload

---
+++ src/main/java/com/tapereader/dao/TipDao.java
@@ -0,0 +1,7 @@
+package com.tapereader.dao;
+
+import com.tapereader.model.ITip;
+
+public interface TipDao extends DataAccess<ITip> {
+
+}
---
+++ src/main/java/com/tapereader/dao/TipDaoDefault.java
@@ -0,0 +1,70 @@
+package com.tapereader.dao;
+
+import java.util.Collection;
+import java.util.List;
+
+import com.tapereader.model.ITip;
+
+public class TipDaoDefault implements TipDao {
+
+    @Override
+    public ITip get(long id) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public ITip load(long id) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public List<ITip> loadAll() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void delete(ITip value) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void deleteAll(Collection<ITip> values) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public boolean deleteById(long id) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public void flush() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public ITip persist(ITip value) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void save(ITip entity) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void saveAll(Collection<ITip> values) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
